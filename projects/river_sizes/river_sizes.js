c29e800a48e850088c0af1f5ffb0db26bdedd7b4
Alex <alexandriaduell@gmail.com>
2023-03-31 14:15:21 -0400

    Continued working on rivers - getting correct sum of total 1s, but not the correct sum of the individual rivers

--- projects/river_sizes/river_sizes.js
+++ projects/river_sizes/river_sizes.js
@@ -3,12 +3,62 @@
     // 1 = river
       // river = horizontal or vertical 1s
         //# of 1s = size of river
-    // return an array of river sizes
-
+    // return an array of river sizes, no particular order
+    
     let n = 0
     let rivers = []
 
+    for(let i = 0; i < matrix.length; i++){
+      for(let r = 0; r < matrix[i].length; r++){
+        if(matrix[i][r] === 1){
+          n += 1
+          matrix[i][r] = 0
+
+        for(let num = 1; num < matrix[i].length; num++){
+            //the right num of matrix[i][r]
+            if(r+num < matrix[i].length){
+              if(matrix[i][r+num] === 1){
+                n += 1
+                matrix[i][r+num] = 0
+              }
+            }
+
+            //the left num of matrix[i][r]
+            if(r-num >= 0){
+              if(matrix[i][r-num] === 1){
+                n += 1
+                matrix[i][r-num] = 0
+              }
+            }
+
+            //the above num of matrix[i][r]
+            if(i+num < matrix.length){ 
+              if(matrix[i+num][r] === 1){
+                n += 1
+                matrix[i+num][r] = 0
+              }
+            }
+
+            //the below num of matrix[i][r]
+            if(i-num >= 0){
+              if(matrix[i-num][r] === 1){
+                n += 1
+                matrix[i-num][r] = 0
+              }
+            }
+          }
+        }
+
+        if(n > 0){
+          rivers.push(n)
+          n = 0
+        }
+      }
+    }  
     
+    console.log(`Rivers:`, rivers)
+
+    return rivers
   };
 
 module.exports = {riverSizes}
--- projects/river_sizes/river_sizes.test.js
+++ projects/river_sizes/river_sizes.test.js
@@ -1,16 +1,19 @@
 const {riverSizes} = require('./river_sizes')
 
 describe('riverSizes', ()=>{
-    it('returns the size of the rivers', ()=>{
+    it('returns the size of the rivers for a matrix[3x4]', ()=>{
         let matrix = [
             [1, 0, 1, 0],
             [0, 1, 1, 1],
             [1, 0, 0, 1],
           ];
           let rivers = riverSizes(matrix);
+          console.log(`Test Rivers Array:`, rivers)
           expect(rivers).toHaveLength(3);
-          expect(rivers).toEqual(expect.arrayContaining([1, 1, 5]))
-          
+          expect(rivers).toBe(expect.arrayContaining([1, 1, 5]))
+        })
+
+      it(`returns the size of the rivers for a matrix[5x5]`, () =>{
           matrix = [
             [1, 0, 0, 1, 0],
             [1, 0, 1, 0, 0],
@@ -21,6 +24,6 @@ describe('riverSizes', ()=>{
           
           rivers = riverSizes(matrix);
           expect(rivers).toHaveLength(5);
-          expect(rivers).toEqual(expect.arrayContaining([1, 2, 2, 2, 5]))
+          expect(rivers).toBe(expect.arrayContaining([1, 2, 2, 2, 5]))
     })
 })
