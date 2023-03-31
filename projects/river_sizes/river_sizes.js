df2a8f7a35044a251d48d404dd0fb19370066a2a
Alex <alexandriaduell@gmail.com>
2023-03-30 22:06:38 -0400

    solve_graph complete + tests pass

--- projects/river_sizes/river_sizes.js
+++ projects/river_sizes/river_sizes.js
@@ -1,5 +1,9 @@
   const riverSizes = (matrix) => {
-
+    // 0 = land
+    // 1 = river
+      // river = horizontal or vertical 1s
+        //# of 1s = size of river
+    // return an array of river sizes
   };
 
 module.exports = {riverSizes}
--- projects/solve_graph/solve_graph.js
+++ projects/solve_graph/solve_graph.js
@@ -1,5 +1,26 @@
 const doesPathExist = (graph, startNode, endNode) => {
+    let visited = []
+    let queue = [startNode]
 
+    while(queue.length > 0){
+        const vert = queue.shift()
+        const neighbors = graph[vert]
+
+        if(graph[vert].includes(endNode)){
+            return true
+        }
+        else if(!graph[vert].includes(endNode)){
+           visited.push(vert)
+
+           neighbors.forEach(neighbor => {
+            if(!visited.includes(neighbor)){
+                queue.push(neighbor)
+            }
+           })
+        }
+    }
+
+    return false
 }
 
 module.exports = {doesPathExist}
